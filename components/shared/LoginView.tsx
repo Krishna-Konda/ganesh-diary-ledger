4adc4a547ccd9023cd9eb207e5c95c6412687894
Krishna Konda <kondakrishna70@gmail.com>
2026-03-29 20:59:07 +0530

    Login page password field hide btn added

--- components/shared/LoginView.tsx
+++ components/shared/LoginView.tsx
@@ -58,8 +58,7 @@ export default function LoginView() {
               <button
                 type="button"
                 onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
-              >
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">
                 {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
               </button>
             </div>
